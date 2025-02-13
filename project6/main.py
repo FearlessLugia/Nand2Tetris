de20e5b32e7bddadb9caacd2ed4f95cb2501b1ec
Kiiro <ajxx030217@163.com>
2025-02-12 19:16:54 -0500

    feat: label & symbol

--- project6/main.py
+++ project6/main.py
@@ -3,36 +3,49 @@ from enum import Enum
 
 # prog.asm -> prog.hack
 class HackAssembler:
-    def __init__(self):
+    def __init__(self, files):
+        self.symbol_table = None
         self.parser = None
         self.code = Code()
-        self.symbol_table = SymbolTable()
+
         self.res = []
+
+        self.files = files
+        self.file_num = -1
+        self.current_file = self.files[0]
 
     # Opens the input file (prog.asm) and gets ready to process it
     # Constructs a symbol table, and adds to it all the predefined symbols
     def initialize(self):
-        with open('MaxL.asm', 'r', encoding="utf-8") as file:
+        with open(self.current_file + '.asm', 'r', encoding="utf-8") as file:
             lines_temp = file.readlines()
 
         lines = []
         for line in lines_temp:
             if not line.strip().startswith('//') and line.strip():
                 lines.append(line.strip())
-        print(lines)
+        print(lines, len(lines))
 
         self.parser = Parser(lines)
+        self.symbol_table = SymbolTable()
 
     # Reads the program lines, one by one, focusing only on (label) declarations.
     # Adds the found labels to the symbol table
     def first_pass(self):
+        line_num_to_reduce = 0
+
         while self.parser.has_more_lines():
             self.parser.advance()
+
             # print(line, self.parser.instruction_type(), self.parser.symbol())
-            if self.parser.instruction_type() != InstructionType.C_INSTRUCTION:
+            if self.parser.instruction_type() == InstructionType.L_INSTRUCTION:
                 symbol = self.parser.symbol()
-                # if not symbol.isdigit():
-                # self.symbol_table.add_entry(symbol, address=)
+                # print('s,num', symbol, self.parser.line_num)
+                if not symbol.isdigit():
+                    self.symbol_table.add_entry(symbol, self.parser.line_num - line_num_to_reduce)
+                    line_num_to_reduce += 1
+
+        print('    symbol_table', self.symbol_table.symbol_table)
 
     # While there are more lines to process:
     #   Gets the next instruction, and parses it
@@ -54,7 +67,7 @@ class HackAssembler:
                 comp = self.parser.comp()
                 jump = self.parser.jump()
                 res = '111' + self.code.comp(comp) + self.code.dest(dest) + self.code.jump(jump)
-                # print(res, line)
+                print('  res', self.parser.current_line, res)
                 self.res.append(res)
 
             elif self.parser.instruction_type() == InstructionType.A_INSTRUCTION:
@@ -62,14 +75,30 @@ class HackAssembler:
                 print('symbol', symbol)
                 if symbol.isdigit():
                     res = str(bin(int(symbol))[2:]).rjust(16, '0')
-                    print('res', res)
                     self.res.append(res)
+                else:
+                    if self.symbol_table.contains(symbol):
+                        address = self.symbol_table.get_address(symbol)
+                        res = str(bin(address)[2:]).rjust(16, '0')
+                        print('  res', res)
+                        self.res.append(res)
+                    else:
+                        self.symbol_table.current_num += 1
+                        self.symbol_table.add_entry(symbol, self.symbol_table.current_num)
 
-            print(self.res)
+            # print(self.res)
+        print('    symbol_table', self.symbol_table.symbol_table)
 
-        with open('MaxL.hack', 'w', encoding="utf-8") as file:
+        with open(self.current_file + '.hack', 'w', encoding="utf-8") as file:
             for line in self.res:
                 file.write(line + '\n')
+
+    def advance(self):
+        self.file_num += 1
+        if self.file_num < len(self.files):
+            self.current_file = self.files[self.file_num]
+        else:
+            self.current_file = None
 
 
 class InstructionType(Enum):
@@ -89,7 +118,7 @@ class Parser:
         self.lines_num = len(lines)
 
     def has_more_lines(self):
-        print('has_more_lines', self.line_num, '<', self.lines_num - 1, self.line_num < self.lines_num - 1)
+        # print('has_more_lines', self.line_num, '<', self.lines_num - 1, self.line_num < self.lines_num - 1)
         return self.line_num < self.lines_num - 1
 
     def advance(self):
@@ -136,8 +165,8 @@ class Code:
                              'D-1': '0001110', 'A-1': '0110010', 'M-1': '1110010', 'D+A': '0000010', 'D+M': '1000010',
                              'D-A': '0010011', 'D-M': '1010011', 'A-D': '0000111', 'M-D': '1000111', 'D&A': '0000000',
                              'D&M': '1000000', 'D|A': '0010101', 'D|M': '1010101'}
-        self.__dest_table = {'null': '000', 'M': '001', 'D': '010', 'DM': '011', 'A': '100', 'AM': '101', 'AD': '110',
-                             'ADM': '111'}
+        self.__dest_table = {'null': '000', 'M': '001', 'D': '010', 'DM': '011', 'MD': '011', 'A': '100', 'AM': '101',
+                             'AD': '110', 'ADM': '111'}
         self.__jump_table = {'null': '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100', 'JNE': '101',
                              'JLE': '110', 'JMP': '111'}
 
@@ -153,7 +182,9 @@ class Code:
 
 class SymbolTable:
     def __init__(self):
-        self.symbol_table = {}
+        self.symbol_table = ({f'R{i}': i for i in range(16)} |
+                             {'SCREEN': 16384, 'KBD': 24576, 'SP': 0, 'LCL': 1, 'ARG': 2, 'THIS': 3, 'THAT': 4})
+        self.current_num = 15
 
     def add_entry(self, symbol, address):
         if not self.contains(symbol):
@@ -167,7 +198,13 @@ class SymbolTable:
 
 
 if __name__ == '__main__':
-    assembler = HackAssembler()
-    assembler.initialize()
-    assembler.first_pass()
-    assembler.second_pass()
+    files = ['Add', 'MaxL', 'Max', 'RectL', 'Rect', 'PongL', 'Pong']
+
+    assembler = HackAssembler(files)
+
+    while assembler.current_file:
+        assembler.initialize()
+        assembler.first_pass()
+        assembler.second_pass()
+
+        assembler.advance()
